89d9d1fb3f52136c75457a8c4433efc8700bbbce
Matt Dillon <mddillon47@gmail.com>
2021-04-03 11:30:25 -0700

    jokes

--- app.js
+++ app.js
@@ -64,3 +64,12 @@ form.addEventListener("submit", function (e) {
   getDogByBreed(input.value);
   input.value = "";
 });
+
+async function getJoke(firstName, lastName) {
+  let res = await axios.get(`http://api.icndb.com/jokes/random`, {
+    params: { firstName, lastName },
+  });
+  console.log(res.data.value.joke);
+}
+
+getJoke('Matt', 'Dillon')
